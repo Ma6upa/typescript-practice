158b15b2ec8eebcbc9db0477b7a97d9354b7b9ca
Ma6upa <vaud55@gmail.com>
2023-05-05 20:56:03 +0400

    add typed axios request

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card, { CardVariant } from './components/Card';
 import UserList from './components/UserList';
 import { IUser } from './types/types';
+import axios from 'axios';
 
 function App() {
-  const users: IUser[] = [
-    {id: 1, name: 'Valera', email: 'yuvalerka@mail.ru', address: {city: 'Izhevsk', street: 'Pushkina', zipcode: '228'}},
-    {id: 2, name: 'Rail', email: 'rail14@mail.ru', address: {city: 'Kazan', street: 'plohaya', zipcode: '1337'}},
-  ]
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+      setUsers(response.data)
+    } catch (e) {
+      alert(e)
+    } finally {
+
+    }
+  }
+
   return (
     <div>
       <Card onClick={(num) => console.log('click', num)} width='200px' height='200px' variant={ CardVariant.outlined }>
