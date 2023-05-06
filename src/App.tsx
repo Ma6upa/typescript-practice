a6808e55ce70f3aaafed205c215abf85855bcaed
Ma6upa <vaud55@gmail.com>
2023-05-05 20:05:50 -0700

    add params to typed prop method in Card component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Card, { CardVariant } from './components/Card';
 function App() {
   return (
     <div>
-      <Card onClick={() => console.log('click')} width='200px' height='200px' variant={ CardVariant.outlined }>
+      <Card onClick={(num) => console.log('click', num)} width='200px' height='200px' variant={ CardVariant.outlined }>
         <button>childrenBtn</button>
         <div>
           <span>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,38 +1,39 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 export enum CardVariant {
-  outlined='outlined',
-  primary='primary'
+  outlined = 'outlined',
+  primary = 'primary'
 }
 
 interface CardProps {
   width?: string;
   height?: string;
   variant: CardVariant;
-  onClick: () => void;
+  onClick: (num: number) => void;
   children?: React.ReactNode;
 }
 
-const Card: FC<CardProps> = 
-  ({ 
-    width, 
+const Card: FC<CardProps> =
+  ({
+    width,
     height,
     variant,
-    onClick, 
-    children, 
+    onClick,
+    children,
   }) => {
-  return(
-    <div style={{
-      width, 
-      height, 
-      border: variant === CardVariant.outlined? '5px solid lightgray' : 'none',
-      background: variant === CardVariant.primary? 'lightgray' : 'white'
-    }}
-    onClick={onClick}
-    >
-      {children}
-    </div>
-  )
-}
+    const [num, setNum] = useState(5)
+    return (
+      <div style={{
+        width,
+        height,
+        border: variant === CardVariant.outlined ? '5px solid lightgray' : 'none',
+        background: variant === CardVariant.primary ? 'lightgray' : 'white'
+      }}
+        onClick={() => onClick(num)}
+      >
+        {children}
+      </div>
+    )
+  }
 
 export default Card;
