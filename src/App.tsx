e0004c00f709133c14dd0c5b0270394467ba6e54
Ma6upa <vaud55@gmail.com>
2023-05-05 19:47:34 -0700

    add children to Card component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import Card from './components/Card';
 function App() {
   return (
     <div>
-      <Card width='200px' height='200px'/>
+      <Card width='200px' height='200px'>
+        <button>childrenBtn</button>
+        <div>
+          <span>
+            childrenText
+          </span>
+        </div>
+      </Card>
     </div>
   );
 }
