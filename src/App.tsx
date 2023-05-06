64429c0a038dc08746295274ce55f4b11b58917f
Ma6upa <vaud55@gmail.com>
2023-05-05 20:20:22 -0700

    add UserList to App with mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
 
 function App() {
+  const users: IUser[] = [
+    {id: 1, name: 'Valera', email: 'yuvalerka@mail.ru', address: {city: 'Izhevsk', street: 'Pushkina', zipcode: '228'}},
+    {id: 2, name: 'Rail', email: 'rail14@mail.ru', address: {city: 'Kazan', street: 'plohaya', zipcode: '1337'}},
+  ]
   return (
     <div>
       <Card onClick={(num) => console.log('click', num)} width='200px' height='200px' variant={ CardVariant.outlined }>
@@ -12,6 +18,7 @@ function App() {
           </span>
         </div>
       </Card>
+      <UserList users={users}/>
     </div>
   );
 }
