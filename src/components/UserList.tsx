71da63a95c002b9b9511028b4d4113a9f657eb4d
Ma6upa <vaud55@gmail.com>
2023-05-05 20:08:48 -0700

    add new component UserList

---
+++ src/components/UserList.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+interface UserListProps {
+  
+}
+
+const UserList: FC<UserListProps> = () => {
+  return (
+    <div>
+      UserList
+    </div>
+  )
+}
+
+export default UserList;
