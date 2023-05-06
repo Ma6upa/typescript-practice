805d6a4773ed82cd73464b69e57a65ed2670976a
Ma6upa <vaud55@gmail.com>
2023-05-05 19:43:22 -0700

    add component with interface on props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Card from './components/Card';
 
 function App() {
   return (
     <div>
-      init
+      <Card width='200px' height='200px'/>
     </div>
   );
 }
